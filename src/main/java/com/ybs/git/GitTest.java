195f04c67928f40f91cb76192bf50055a606a430
ybs <2668879709@qq.com>
2022-02-19 13:52:23 +0800

    This is my third commit

--- src/main/java/com/ybs/git/GitTest.java
+++ src/main/java/com/ybs/git/GitTest.java
@@ -7,5 +7,7 @@ package com.ybs.git;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello,Git!");
+        System.out.println("hello,Git2!");
+        System.out.println("hello,Git3!");
     }
 }
