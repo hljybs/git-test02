d22fb31da8ad90f665f1e72030b4151e06095b2e
ybs <2668879709@qq.com>
2022-02-19 13:48:59 +0800

    This is my first commit

---
+++ src/main/java/com/ybs/git/GitTest.java
@@ -0,0 +1,11 @@
+package com.ybs.git;
+
+/**
+ * @author ybs
+ * @create 2022-02-19-13:45
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello,Git!");
+    }
+}
